fc17404ec37206a5ec6d667f15e0d8ffdd31aa3a
SgtGarro <marcelogarro137@gmail.com>
2024-10-18 02:03:58 -0500

    build: create boilerplate for development

---
+++ android/app/src/main/kotlin/com/homeypark/homey_park_mobile_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.homeypark.homey_park_mobile_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import 'util.dart';
+import 'theme.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final brightness = View.of(context).platformDispatcher.platformBrightness;
+    TextTheme textTheme = createTextTheme(context, "Rubik", "Rubik");
+
+    MaterialTheme theme = MaterialTheme(textTheme);
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: brightness == Brightness.light ? theme.light() : theme.dark(),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.primary,
+        title: Text(widget.title, style: TextStyle(color: Theme.of(context).colorScheme.onPrimary)),
+      ),
+      body: const Center(
+      ),
+
+    );
+  }
+}
+
